b19e72bde4fc52f346fa9e0e56bcb9f9b6711426
andre-sarmento <andresarmento2@hotmail.com>
2023-03-03 10:31:18 -0300

    Ajuste de Endereço

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { ChatClient } from './chat-client';
 
-const URL = 'wss://09xqig1wrd.execute-api.us-east-2.amazonaws.com/production/';
+const URL = 'wss://b9iq50fcni.execute-api.us-west-2.amazonaws.com/ChatBot';
 
 const App = () => {
   const socket = useRef<WebSocket | null>(null);
